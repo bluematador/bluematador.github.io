5dddee6505f1c329093ad6f52d7deff7528ac99d
Keilan Jackson <keilan@bluematador.com>
2019-08-26 14:44:56 -0600

    Release new docs

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 set -o errexit #abort if any command fails
-bundle exec middleman build --clean
+
+# probably need dependencies for this to work
+# sudo apt-get install ruby ruby-dev zlib1g-dev liblzma-dev
+# gem upate --system
+# bundle install
+
+sudo bundle exec middleman build --clean
 cp -r build/* .
 rm -fr build
